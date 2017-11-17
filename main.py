b4dfcb0392804398caa9767d29d235e520c52853
nicobrubaker <09bridge>
2017-11-16 18:31:03 -0800

    changed main.py to text only

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ players[1] = int(input("Enter the AI number for the second team: "))
 players[2] = int(input("Enter the AI number for the third team: "))
 players[3] = int(input("Enter the AI number for the fourth team: "))
 players[4] = int(input("Enter the AI number for the fifth team: "))
+print("\nPositions at each stage in the race, followed by available funds.\n")
 
 place = 1
 rankings = [0] * 15
@@ -38,65 +39,9 @@ positions = [0] * 15
 funds = [1000000] * 5
 teamscores = [0] * 5
 
-import pygame
-
-pygame.init()
-screen = pygame.display.set_mode((1000, 650))
-background = pygame.Surface((1000, 650))
-smallfont = pygame.font.SysFont('arial', 20)
-medfont = pygame.font.SysFont('copperplate', 40)
-largefont = pygame.font.SysFont('georgia', 60)
-
-## DRAW BACKGROUND
-
-colors = [(50, 200, 200), (250, 125, 0), (50, 50, 200), (50, 200, 50), (200, 50, 200)]
-
-for j in range(0, 151):
-    for k in range(j % 2, 20, 2):
-        pygame.draw.rect(background, (200, 200, 200), (j * 4, k * 4, 4, 4), 0)
-title = pygame.image.load("thedash.png").convert_alpha()
-background.blit(title, (176, 5))
-for j in range(0, 5):
-    pygame.draw.rect(background, (120, 120, 120), (42 + 120 * j, 77, 40, 540), 3)
-pygame.draw.rect(background, (255, 215, 0), (2, 2, 600, 660), 5)
-pygame.draw.rect(background, (255, 215, 0), (2, 77, 600, 30), 5)
-pygame.draw.rect(background, (255, 215, 0), (122, 77, 120, 600), 5)
-pygame.draw.rect(background, (255, 215, 0), (362, 77, 120, 600), 5)
-pygame.draw.rect(background, (255, 215, 0), (2, 588, 600, 30), 5)
-for j in range(0, 5):
-    text = medfont.render(names[players[j]], 1, colors[j], (0, 0, 0))
-    background.blit(text, (630, 25 + 120 * j))
-
-
-def display():
-    screen.blit(background, (0, 0))
-    for j in range(0, 5):
-        text = smallfont.render('$' + str(max(funds[j], 0)), 1, (255, 255, 255), (0, 0, 0))
-        width = text.get_width()
-        screen.blit(text, (120 * j + 62 - int(width / 2), 623))
-    for j in range(0, 5):
-        for k in range(0, 3):
-            pygame.draw.circle(screen, colors[j], (23 + 39 * k + 120 * j, 603 - int(511 * positions[3 * j + k] / 100)),
-                               8, 0)
-            pygame.draw.circle(screen, (255, 255, 255),
-                               (23 + 39 * k + 120 * j, 603 - int(511 * positions[3 * j + k] / 100)), 9, 1)
-            if rankings[3 * j + k] > 0:
-                text = smallfont.render(str(rankings[3 * j + k]), 1, (255, 255, 255), (0, 0, 0))
-                width = text.get_width()
-                screen.blit(text, (120 * j + 39 * k + 22 - int(width / 2), 112))
-    for j in range(0, 5):
-        text = largefont.render(str(teamscores[j]), 1, (200, 200, 200), (0, 0, 0))
-        width = text.get_width()
-        screen.blit(text, (850 - width, 55 + 120 * j))
-    pygame.display.flip()
-
-
 ##############################
 ## CONTROLLER RUNS THE RACE ##
 ##############################
-
-display()
-pygame.time.wait(500)
 
 ## GATHER BIDS, CLEAN UP VALUES
 
@@ -147,32 +92,10 @@ while funds[0] >= 0 or funds[1] >= 0 or funds[2] >= 0 or funds[3] >= 0 or funds[
 
                 ## ADVANCE RUNNERS, DEBIT ACCOUNTS, ASSIGN RANKS AS RUNNERS FINISH
 
-    increments = [0] * 15
-
     if longwinbid >= 0:
         index = int(longindex / 3)
         funds[index] -= longwinbid
-        increments[longindex] = min(distances[2], 100 - positions[longindex])
-    if mediumwinbid >= 0:
-        index = int(mediumindex / 3)
-        funds[index] -= mediumwinbid
-        increments[mediumindex] = min(distances[1], 100 - positions[mediumindex])
-    if shortwinbid >= 0:
-        index = int(shortindex / 3)
-        funds[index] -= shortwinbid
-        increments[shortindex] = min(distances[0], 100 - positions[shortindex])
-
-    for j in range(0, 15):
-        increments[j] = increments[j] / 100
-    for k in range(0, 100):
-        for j in range(0, 15):
-            positions[j] += increments[j]
-        display()
-    for j in range(0, 15):
-        positions[j] = int(positions[j] + 0.5)
-
-    if longwinbid >= 0:
-        index = int(longindex / 3)
+        positions[longindex] = min(positions[longindex] + distances[2], 100)
         if positions[longindex] == 100 and rankings[longindex] == 0:
             rankings[longindex] = place
             place += 1
@@ -180,6 +103,8 @@ while funds[0] >= 0 or funds[1] >= 0 or funds[2] >= 0 or funds[3] >= 0 or funds[
                 funds[index] = -1
     if mediumwinbid >= 0:
         index = int(mediumindex / 3)
+        funds[index] -= mediumwinbid
+        positions[mediumindex] = min(positions[mediumindex] + distances[1], 100)
         if positions[mediumindex] == 100 and rankings[mediumindex] == 0:
             rankings[mediumindex] = place
             place += 1
@@ -187,6 +112,8 @@ while funds[0] >= 0 or funds[1] >= 0 or funds[2] >= 0 or funds[3] >= 0 or funds[
                 funds[index] = -1
     if shortwinbid >= 0:
         index = int(shortindex / 3)
+        funds[index] -= shortwinbid
+        positions[shortindex] = min(positions[shortindex] + distances[0], 100)
         if positions[shortindex] == 100 and rankings[shortindex] == 0:
             rankings[shortindex] = place
             place += 1
@@ -200,19 +127,7 @@ while funds[0] >= 0 or funds[1] >= 0 or funds[2] >= 0 or funds[3] >= 0 or funds[
                 score += 100 - (rankings[3 * j + k] * (rankings[3 * j + k] - 1)) / 2
         teamscores[j] = int(score)
 
-    display()
-    pygame.time.wait(200)
+    print(positions, funds)
 
-################################################
-## USER WILL PRESS ESC TO CLOSE PYGAME WINDOW ##
-################################################
-
-mainloop = True
-while mainloop:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            mainloop = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                mainloop = False
-pygame.quit()
+print("\nFinal scores for each of the five teams.\n")
+print(teamscores)
