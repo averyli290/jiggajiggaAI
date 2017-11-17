c4e21839f355f5c908308356ab9e259326b1c536
averyli290 <31978780+averyli290@users.noreply.github.com>
2017-11-16 22:11:55 -0800

    Update ai.py

--- ai.py
+++ ai.py
@@ -4,9 +4,13 @@ def ai(pos, funds, dist):
     import random
     valuepermeter = funds[0]/(350-pos[0]-pos[1]-pos[2])
     # to counter steadyfreddy?
+    
     if valuepermeter <= 3333 and pos[0] < 25 or pos[1] < 25 or pos[2] < 25:
         valuepermeter = 3333 + random.randint(25, 300)
-    
+    '''
+    if valuepermeter <= 3333 and pos[0] < 50 or pos[1] < 50 or pos[2] < 50:
+        valuepermeter = 3333 + random.randint(25, 300)
+    '''
     smallbid = valuepermeter*dist[0]
     midbid = valuepermeter*dist[1]
     bigbid = valuepermeter*dist[2]
